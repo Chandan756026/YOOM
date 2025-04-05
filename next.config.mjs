1b65eeca627364acfa1c15982a6e2c1cc50ea5ac
Chandan756026 <chandangiri30968@gmail.com>
2025-04-05 17:55:31 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['img.clerk.com'], // Allow Clerk profile images
-  },
-  reactStrictMode: true,
-  experimental: {
-    serverActions: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com',
+      },
+    ],
   },
 };
 
